a853be1d3d417ad8dbf18be9bdd28ade8cb3af9a
Ryan Andrews <rcandrews44@gmail.com>
2021-04-13 19:17:30 -0400

    commit tree 908372c9bf3c660da58a32124182aa896063cb93

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-const cats = ["Milo", "Otis", "Garfield"]
+let cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name) {
-    cats.push(name);
+function destructivelyAppendCat(name){
+    return cats.push(name);
 }
 
-function destructivelyPrependCat(name) {
-    cats.unshift(name);
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(name) {
-    cats.pop(name);
+function destructivelyRemoveLastCat(){
+    return cats.pop();
 }
 
-function destructivelyRemoveFirstCat(name) {
-    cats.shift(name);
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
 }
 
-function appendCat(name) {
+function appendCat(name){
     return [...cats, name];
 }
 
-function prependCat(name) {
-    return [name, ...cats];
+function prependCat(name){
+    return [name, ...cats]
 }
 
-function removeLastCat() {
-    return cats.slice(0,2);
-    
-}
-
-function removeFirstCat() {
+function removeFirstCat(){
     return cats.slice(1);
+}
+
+function removeLastCat(){
+    return cats.slice(0,2);
 }
